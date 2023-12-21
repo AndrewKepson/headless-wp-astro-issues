ebe57861a8181f004e49ba712a3940703987199f
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2023-12-21 15:36:52 -0700

    custom base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  base: "/custom-base",
+  trailingSlash: "never",
+  build: {
+    assetsPrefix: import.meta.env.BASE_URL,
+  },
   adapter: netlify(),
 });
